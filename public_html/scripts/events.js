5d8ffc050649c7d7d28d83e943b875ca1c443e07
computer7 <computer7@computer7>
2014-02-17 15:42:44 +0530

    Settimeout focus

--- public_html/scripts/events.js
+++ public_html/scripts/events.js
@@ -3,6 +3,14 @@ $(document).on("pagebeforeshow", "#searchPatient", Patients.initView);
 $(document).on("pagebeforeshow", "#patientDetail", PatientDetail.initView);
 $(document).on("pagebeforeshow", "#requirements", Requirement.initView);
 $(document).delegate("#searchPatient", "pageinit", Patients.init).delegate("#login", "pageinit", Login.init).delegate("#patientDetail", "pageinit", PatientDetail.init).delegate("#requirements", "pageinit", Requirement.init);
+$(document).on("pageshow", "#login", function() {
+    $("#username").focus();
+    plugins.SoftKeyBoard.show(function () {
+        alert("success");
+    },function () {
+       alert("Fail");
+    });
+});
 $(document).on("pageshow", "#searchPatient", function() {
     $("#patientSearch").select();
     $("#patientSearch").focus();
